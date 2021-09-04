0defdd85dedb3a607d9d6ce7a1e8c16c86ee5e27
Nguyen Van Huyen <lockex1987@gmail.com>
2021-09-04 09:59:51 +0700

    Lay noi dung tu file

--- tests/EpubCreatorTest.php
+++ tests/EpubCreatorTest.php
@@ -9,22 +9,9 @@ class EpubCreatorTest extends TestCase
 {
     public function testCreateEpub(): void
     {
-        $content = <<<HTML
-            <?xml version="1.0" encoding="utf-8"?>
-            <!DOCTYPE html>
-            <html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops">
-            <head>
-                <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-                <title>Test title</title>
-                <link href="../css/style.css" rel="stylesheet" type="text/css" />
-            </head>
-                <p>Test content</p>
-            </html>
-            HTML;
-        
+        $content = file_get_contents('assets/chapter-1.html');
         $title = 'Thư Kiếm Ân Cừu Lục';
         $author = 'Kim Dung';
-
         $chapters = [
             [
                 'name' => 'Hồi 1: Núi hoang hào kiệt trừ ưng cẩu',
