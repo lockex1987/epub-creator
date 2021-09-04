0d02c7a3d7cd5e4850f6b250a0e655032e83519b
Nguyen Van Huyen <lockex1987@gmail.com>
2021-09-04 09:54:59 +0700

    Thuc thi test

--- tests/EpubCreatorTest.php
+++ tests/EpubCreatorTest.php
@@ -9,13 +9,26 @@ class EpubCreatorTest extends TestCase
 {
     public function testCreateEpub(): void
     {
+        $content = <<<HTML
+            <?xml version="1.0" encoding="utf-8"?>
+            <!DOCTYPE html>
+            <html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops">
+            <head>
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+                <title>Test title</title>
+                <link href="../css/style.css" rel="stylesheet" type="text/css" />
+            </head>
+                <p>Test content</p>
+            </html>
+            HTML;
+        
         $title = 'Thư Kiếm Ân Cừu Lục';
         $author = 'Kim Dung';
 
         $chapters = [
             [
-                'name' => 'Hồi 1: Núi hoang hào kiệt trừ ưng cẩu, Dọc đường tỷ kiếm gặp anh hùng',
-                'content' => 'Test'
+                'name' => 'Hồi 1: Núi hoang hào kiệt trừ ưng cẩu',
+                'content' => $content
             ]
         ];
         $coverPath = 'assets/cover.jpg';
